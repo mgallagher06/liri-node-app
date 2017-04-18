9fc5b8f1859e13f8c20db84a7427a3a416867039
Meagan M Gallagher <meaganmgallagher@Meagans-MacBook-Pro.local>
2017-04-17 20:04:07 -0400

    more changes

--- liri.js
+++ liri.js
@@ -5,18 +5,14 @@ var request = require('request');
 
 spotify.search({ type: 'track', query: 'dancing in the moonlight' },
 	function(err, data) {
-    if ( err ) {
-        console.log('Error occurred: ' + err);
-        (data)	{
-    		console.log (data);
-    	}
-
-    	}
-    });
-// // node liri.js do-what-it-says
-// //var request = require('request');
-// request('random.txt', function (error, response, body) {
-//   console.log('error:', error); // Print the error if one occurred
-//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-//   console.log('body:', body); // Print the HTML for the Google homepage.
-// });
+  	if ( err ) {
+      console.log('Error occurred: ' + err);
+		}
+		else {
+  		console.log (data.tracks.items);
+			var items = data.tracks.items
+  	}
+		for (i = 0; i < items.length; i++) {
+    	console.log(items[i].artist);
+		}
+  });
